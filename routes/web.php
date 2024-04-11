859a5c743e5c13be7214388f38834957380a9c65
Slandio Soares <lizandroelis79@gmail.com>
2024-04-11 02:24:21 +0100

    Reorganização de alguns controllers

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\StartController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\CadastrarController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [StartController::class, 'start']);
 
-Route::get('/login', [LoginController::class, 'login']);
+Route::get('/login', [UserController::class, 'login']);
 
-Route::get('/cadastrar', [CadastrarController::class, 'cadastrar']);
+Route::get('/cadastrar', [UserController::class, 'cadastrar']);
