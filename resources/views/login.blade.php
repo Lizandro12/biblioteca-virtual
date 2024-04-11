ba23bbddb036ac6e75b8fe9e8393f4bfcab39518
Slandio Soares <lizandroelis79@gmail.com>
2024-04-11 17:46:30 +0100

    alterações

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,9 +10,31 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="/public/css/login.css">
 </head>
 <body>
-    
+    <div class="content">
+        <div class="left-side col-md-6">
+            <form action="" class="form">
+                <div class="header-form">
+                    <h1>Bem-vindo!</h1>
+                    <p>Primeira vez aqui? <a href="/cadastrar" class="header-form-link">Inscrever-se</a></p>
+                </div>
+                <div class="input-group mb-3">
+                    <label for="email">E-mail:</label>
+                    <input type="text" name="email" id="email" class="form-control">
+                </div>
+                <div class="input-group mb-3">
+                    <label for="password">Senha:</label>
+                    <input type="text" name="password" id="password" class="form-control">
+                </div>
+                <input type="submit" value="Entrar" class="btn btn-primary">
+            </form>
+        </div>
+        <div class="rigth-side"></div>
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- Biblioteca de icones -->
