42a54afb6e29cf5b1856f58f6d004081883cbe0f
HelioSant <165045620+HelioSant@users.noreply.github.com>
2024-05-10 17:02:20 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\StartController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SearchController;
 
 Route::get('/', [StartController::class, 'start']);
+Route::get('/search', [StartController::class, 'search']);
 Route::get('/login', [UserController::class, 'login']);
 Route::get('/cadastrar', [UserController::class, 'cadastrar']);
 Route::get('/recover', [UserController::class, 'recoverpassword']);
 Route::get('/recover/password', [UserController::class, 'newpassword']);
+
