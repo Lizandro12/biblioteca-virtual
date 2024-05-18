13950d2572814b9c9135771b1d87c8087866d824
Slandio Soares <slandio.soares@positiva.co.ao>
2024-05-18 22:30:30 +0100

    Comentarios para modificaçoes e correçoes

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -31,12 +31,13 @@
 </head>
 <body >
 
-        <nav class="navbar">
+    <header>
+        <div class="header-top">
             <div class="navbar-logo">
                 <img src="/img/logotipo.png" alt="Logo">
             </div>
-            <div class="navbar-search">
-                <input style="background-color:#DFDFDF"type="text" placeholder="">
+            <div class="navbar-search d-flex"><!-- Remover este estilo inline e todos os outros que existem nesta pagine e enviar para o arquivo de estilos-->
+                <input style="background-color:#DFDFDF"type="text" placeholder="Procure por um livro..." class="form-control">
                 <button class="btn btn-orange">
                     <i class="fas fa-search"></i>
                 </button>
@@ -45,43 +46,41 @@
                 <a href="/login"><button class="btn1">Entrar</button></a>
                 <a href="/cadastrar"><button class="btn2">Cadastrar</button></a>
             </div>
-        </nav>
-
-
-<!--segunda barra ----------------------------------------------------------------------- -->
-
-      <div class="header">
-        <nav class="navbar navbar-expand-md navbar-light bg-dark">
-            <div class="container">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white;">Início</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white;">Livros</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white;">Autores</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white;">Categorias</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white;">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link"><i class="bi-cart3" style="color: white;"></i></a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
+        </div>
+            <!--segunda barra ----------------------------------------------------------------------- -->
+          <div class="header">
+            <nav class="navbar navbar-expand-md navbar-light bg-dark"> <!-- Mudar a cor desta seçao para a cor do prototipo no figma -->
+                <div class="container">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="color: white;">Início</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="color: white;">Livros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="color: white;">Autores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="color: white;">Categorias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="color: white;">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link"><i class="bi-cart3" style="color: white;"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
 <!------------------------------------------------------------------------------------------------------------------->
 
 
 <!-- Corpo da página -->
-
-<div style="margin-top:60px"class="textbody">
+<!-- Envolver  -->
+<div style="margin-top:60px"class="textbody"><!-- Remover este estilo inline e colocar no arquivo de estilos -->
     <h3> Todos os Livros</h3><hr>
     <h5> Procure pelo livro do seu interesse</h5>
 </div>
@@ -89,16 +88,16 @@
 
 <div class="list">
     <div class="pes">
-        <h3 style="padding-top:40px;padding-left:10px;">Pesquisar</h3>
+        <h3 style="padding-top:40px;padding-left:10px;">Pesquisar</h3><!-- Remoer estilo inline -->
         <div class="navbar-search">
-            <input style="background-color:#DFDFDF" type="text" placeholder="">
+            <input style="background-color:#DFDFDF" type="text" placeholder="">Adicionar placeholder
             <button class="btn btn-orange">
                 Filtrar
             </button>
         </div>
         <hr>
 
-        <br><br><br><br><br><br><br><br><br><br><hr>
+        <br><br><br><br><br><br><br><br><br><br><hr><!-- Remover este espaçamento com br e usar o flex box junto com algumas divs pra deixar mais facil de manter -->
         <br><br><br><br><br><br><br><br><br><br><hr>
         <br><br><br><br><br><br><br><br><br><br><hr>
         <br><br><br><br><br><br><hr>
@@ -204,20 +203,35 @@
             </div>
           </div>
 
+          <div class="liv">
+            <div class="row book-row">
+              <div class="col-md-3 book-cover">
+                <img src="/img/capahtml.jpeg" style="height:150px"alt="Capa do Livro">
+              </div>
+              <div class="col-md-3 book-details">
+                <p><strong>Autor:</strong> Nome do Autor</p>
+                <p><strong>Título:</strong> Título do Livro</p>
+                <p><strong>Editora:</strong> Nome da Editora</p>
+              </div>
+              <div class="col-md-3"></div>
+              <div class="col-md-3 book-actions">
+                <p><strong>2000 AOA</strong></p>
+                <button class="bt bt-primar" style="width: 160px;height:30px;">Pagar</button>
+                <button class="bt bt-secondar"style="width: 160px;height:30px;">Ver Detalhes</button>
+              </div>
+            </div>
+          </div>
+
 
         </div>
 </div>
-
-
-
-
-
 
 <div class="containerca">
     <ul class="pagination justify-content-end">
       <li class="page-item ">
         <a class="page-link" href="#" aria-label="Previous">
-          <i class="bi bi-chevron-left fs-4"></i></a>
+            <i class="bi bi-chevron-left fs-4"></i>
+        </a>
       </li>
       <li class="page-item"><a class="page-link" href="#">1</a></li>
       <li class="page-item"><a class="page-link" href="#">2</a></li>
@@ -229,11 +243,6 @@
     </ul>
   </div>
 
-
-
-<!------------------------------------------------------------------------------------------------------------------->
-
-
     <!-- Rodapé -->
     <div class="footer">
         <div class="foo">
@@ -241,6 +250,7 @@
                 <!-- Primeira coluna -->
                 <div class="col-md-4">
                     <h3>Institucional</h3>
+                    <!-- Adicionar esses itens em links -->
                     <p>Quem somos</p>
                     <p>Direitos Autorias</p>
                     <p>Blog</p>
